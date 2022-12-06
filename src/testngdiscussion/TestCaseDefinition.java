f9f83f8c4eadbde1e587a0e17afb6c889c999e6b
velocity.ctc@gmail.com <velocity.ctc@gmail.com>
2022-12-06 10:55:46 +0530

    Testng

---
+++ src/testngdiscussion/TestCaseDefinition.java
@@ -0,0 +1,22 @@
+package testngdiscussion;
+
+import org.testng.annotations.Test;
+
+public class TestCaseDefinition {
+	
+	
+	@Test
+	public void testCase1()
+	{
+		System.out.println("Test case 1");
+	}
+	
+	
+	@Test
+	public void testCase2()
+	{
+		System.out.println("Test case 2");
+	}
+	
+
+}
